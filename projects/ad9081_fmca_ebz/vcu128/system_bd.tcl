zhang-jinyu/hdl

ad9081_fmca_ebz/vcu128: Use HBM for data offload cores

projects/ad9081_fmca_ebz/vcu128/system_bd.tcl
@@ -4,11 +4,20 @@ set adc_fifo_samples_per_converter [expr $ad_project_params(RX_KS_PER_CHANNEL)*1
 ## DAC FIFO depth in samples per converter
 set dac_fifo_samples_per_converter [expr $ad_project_params(TX_KS_PER_CHANNEL)*1024]
 
+source $ad_hdl_dir/library/util_hbm/scripts/adi_util_hbm.tcl
+ad_create_hbm HBM
+
 source $ad_hdl_dir/projects/common/vcu128/vcu128_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 source $ad_hdl_dir/projects/ad9081_fmca_ebz/common/ad9081_fmca_ebz_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+
+ad_connect_hbm HBM mxfe_rx_data_offload/i_util_hbm $sys_hbm_clk $sys_hbm_resetn 0
+ad_connect_hbm HBM mxfe_tx_data_offload/i_util_hbm $sys_hbm_clk $sys_hbm_resetn 4
+
+ad_connect HBM/HBM_REF_CLK_0 $sys_cpu_clk
+ad_connect HBM/APB_0_PCLK $sys_cpu_clk
+ad_connect HBM/APB_0_PRESET_N $sys_cpu_resetn
 
 ad_ip_parameter axi_mxfe_rx_jesd/rx CONFIG.NUM_INPUT_PIPELINE 2
 ad_ip_parameter axi_mxfe_tx_jesd/tx CONFIG.NUM_OUTPUT_PIPELINE 1

tausen/adi-hdl

projects/ad9081_fmca_ebz/vcu128/system_bd.tcl
@@ -4,11 +4,20 @@ set adc_fifo_samples_per_converter [expr $ad_project_params(RX_KS_PER_CHANNEL)*1
 ## DAC FIFO depth in samples per converter
 set dac_fifo_samples_per_converter [expr $ad_project_params(TX_KS_PER_CHANNEL)*1024]
 
+source $ad_hdl_dir/library/util_hbm/scripts/adi_util_hbm.tcl
+ad_create_hbm HBM
+
 source $ad_hdl_dir/projects/common/vcu128/vcu128_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 source $ad_hdl_dir/projects/ad9081_fmca_ebz/common/ad9081_fmca_ebz_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+
+ad_connect_hbm HBM mxfe_rx_data_offload/storage_unit $sys_hbm_clk $sys_hbm_resetn 0
+ad_connect_hbm HBM mxfe_tx_data_offload/storage_unit $sys_hbm_clk $sys_hbm_resetn 4
+
+ad_connect HBM/HBM_REF_CLK_0 $sys_cpu_clk
+ad_connect HBM/APB_0_PCLK $sys_cpu_clk
+ad_connect HBM/APB_0_PRESET_N $sys_cpu_resetn
 
 ad_ip_parameter axi_mxfe_rx_jesd/rx CONFIG.NUM_INPUT_PIPELINE 2
 ad_ip_parameter axi_mxfe_tx_jesd/tx CONFIG.NUM_OUTPUT_PIPELINE 1