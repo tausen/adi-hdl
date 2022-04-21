BluSDRer/hdl

ad9695: Add reference design for ad9695 eval board
Signed-off-by: Liviu.Iacob <liviu.iacob@analog.com>

projects/ad9695_fmc/common/ad9695_fmc_bd.tcl
@@ -1,0 +1,161 @@
+
+# RX parameters
+set RX_NUM_OF_LANES $ad_project_params(RX_JESD_L)           ; # L
+set RX_NUM_OF_CONVERTERS $ad_project_params(RX_JESD_M)      ; # M
+set RX_SAMPLES_PER_FRAME $ad_project_params(RX_JESD_S)      ; # S
+set RX_SAMPLE_WIDTH 16                                      ; # N/NP
+set RX_SAMPLES_PER_CHANNEL [expr $RX_NUM_OF_LANES * 32 / ($RX_NUM_OF_CONVERTERS * $RX_SAMPLE_WIDTH)]
+
+set adc_data_width [expr $RX_SAMPLE_WIDTH * $RX_NUM_OF_CONVERTERS * $RX_SAMPLES_PER_CHANNEL]
+set adc_dma_data_width 128
+
+source $ad_hdl_dir/library/jesd204/scripts/jesd204.tcl
+
+# adc peripherals
+
+ad_ip_instance axi_adxcvr axi_ad9695_rx_xcvr [list \
+  NUM_OF_LANES $RX_NUM_OF_LANES \
+  QPLL_ENABLE 1 \
+  TX_OR_RX_N 0 \
+  SYS_CLK_SEL 3 \
+  OUT_CLK_SEL 3 \
+  ]
+
+adi_axi_jesd204_rx_create axi_ad9695_rx_jesd $RX_NUM_OF_LANES
+ad_ip_parameter axi_ad9695_rx_jesd/rx CONFIG.SYSREF_IOB false
+
+ad_ip_instance util_cpack2 util_ad9695_rx_cpack [list \
+  NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS \
+  SAMPLES_PER_CHANNEL $RX_SAMPLES_PER_CHANNEL \
+  SAMPLE_DATA_WIDTH $RX_SAMPLE_WIDTH \
+  ]
+
+adi_tpl_jesd204_rx_create rx_ad9695_tpl_core $RX_NUM_OF_LANES \
+                                               $RX_NUM_OF_CONVERTERS \
+                                               $RX_SAMPLES_PER_FRAME \
+                                               $RX_SAMPLE_WIDTH
+
+ad_ip_instance clk_wiz dma_clk_wiz
+ad_ip_parameter dma_clk_wiz CONFIG.PRIMITIVE MMCM
+ad_ip_parameter dma_clk_wiz CONFIG.RESET_TYPE ACTIVE_LOW
+ad_ip_parameter dma_clk_wiz CONFIG.USE_LOCKED false
+ad_ip_parameter dma_clk_wiz CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 332.9
+ad_ip_parameter dma_clk_wiz CONFIG.PRIM_SOURCE No_buffer
+
+ad_ip_instance axi_dmac axi_ad9695_rx_dma [list \
+  DMA_TYPE_SRC 2 \
+  DMA_TYPE_DEST 0 \
+  CYCLIC 0 \
+  SYNC_TRANSFER_START 0 \
+  DMA_2D_TRANSFER 0 \
+  FIFO_SIZE 32 \
+  MAX_BYTES_PER_BURST 2048 \
+  AXI_SLICE_DEST 1 \
+  AXI_SLICE_SRC 1 \
+  DMA_LENGTH_WIDTH 24 \
+  DMA_DATA_WIDTH_DEST $adc_dma_data_width \
+  DMA_DATA_WIDTH_SRC $adc_dma_data_width \
+  ]
+
+# common cores
+# fPLLClkin = 325 MHz => RX_CLK25_DIV = 13
+# fPLLClkout = 13000 MHz - qpll0
+
+ad_ip_instance util_adxcvr util_ad9695_xcvr [list \
+  RX_NUM_OF_LANES $RX_NUM_OF_LANES \
+  TX_NUM_OF_LANES 0 \
+  QPLL_FBDIV 40 \
+  QPLL_REFCLK_DIV 1 \
+  RX_OUT_DIV 1 \
+  RX_CLK25_DIV 13 \
+  POR_CFG 0x0 \
+  QPLL_CFG0 0x331c \
+  QPLL_CFG1 0xd038 \
+  QPLL_CFG1_G3 0xd038 \
+  QPLL_CFG2 0xfc1 \
+  QPLL_CFG2_G3 0xfc1 \
+  QPLL_CFG3 0x120 \
+  QPLL_CFG4 0x4 \
+  QPLL_CP 0xff \
+  QPLL_CP_G3 0xf \
+  QPLL_LPF 0x33f \
+  CH_HSPMUX 0x4444 \
+  PREIQ_FREQ_BST 1 \
+  RXPI_CFG0 0x104 \
+  RXPI_CFG1 0x0 \
+  RXCDR_CFG0 0x3 \
+  RXCDR_CFG2_GEN2 0x265 \
+  RXCDR_CFG2_GEN4 0x164 \
+  RXCDR_CFG3 0x18 \
+  RXCDR_CFG3_GEN2 0x18 \
+  RXCDR_CFG3_GEN3 0x18 \
+  RXCDR_CFG3_GEN4 0x12 \
+  ]
+
+# xcvr interfaces
+
+set rx_ref_clk     rx_ref_clk_0
+
+create_bd_port -dir I $rx_ref_clk
+create_bd_port -dir I rx_core_clk_0
+
+ad_connect  $sys_cpu_resetn util_ad9695_xcvr/up_rstn
+ad_connect  $sys_cpu_clk util_ad9695_xcvr/up_clk
+
+# Rx
+ad_connect ad9695_rx_device_clk rx_core_clk_0
+ad_xcvrcon  util_ad9695_xcvr axi_ad9695_rx_xcvr axi_ad9695_rx_jesd {} ad9695_rx_device_clk
+ad_xcvrpll $rx_ref_clk util_ad9695_xcvr/qpll_ref_clk_0
+for {set i 0} {$i < $RX_NUM_OF_LANES} {incr i} {
+  set ch [expr $i]
+  ad_xcvrpll  $rx_ref_clk util_ad9695_xcvr/cpll_ref_clk_$ch
+  ad_xcvrpll  axi_ad9695_rx_xcvr/up_pll_rst util_ad9695_xcvr/up_cpll_rst_$ch
+}
+
+ad_xcvrpll  axi_ad9695_rx_xcvr/up_pll_rst util_ad9695_xcvr/up_qpll_rst_*
+
+# connections (adc)
+
+ad_connect $sys_cpu_resetn axi_ad9695_rx_dma/m_dest_axi_aresetn
+ad_connect dma_clk_wiz/clk_out1 axi_ad9695_rx_dma/m_dest_axi_aclk
+ad_connect util_ad9695_rx_cpack/packed_fifo_wr axi_ad9695_rx_dma/fifo_wr
+
+ad_connect $sys_cpu_clk dma_clk_wiz/clk_in1
+ad_connect $sys_cpu_resetn dma_clk_wiz/resetn
+
+ad_connect ad9695_rx_device_clk rx_ad9695_tpl_core/link_clk
+ad_connect ad9695_rx_device_clk util_ad9695_rx_cpack/clk
+ad_connect ad9695_rx_device_clk axi_ad9695_rx_dma/fifo_wr_clk
+ad_connect ad9695_rx_device_clk_rstgen/peripheral_reset util_ad9695_rx_cpack/reset
+
+ad_connect axi_ad9695_rx_jesd/rx_sof rx_ad9695_tpl_core/link_sof
+ad_connect axi_ad9695_rx_jesd/rx_data_tdata rx_ad9695_tpl_core/link_data
+ad_connect axi_ad9695_rx_jesd/rx_data_tvalid rx_ad9695_tpl_core/link_valid
+
+ad_connect rx_ad9695_tpl_core/adc_valid_0 util_ad9695_rx_cpack/fifo_wr_en
+ad_connect rx_ad9695_tpl_core/adc_enable_0 util_ad9695_rx_cpack/enable_0
+ad_connect rx_ad9695_tpl_core/adc_enable_1 util_ad9695_rx_cpack/enable_1
+ad_connect rx_ad9695_tpl_core/adc_data_0 util_ad9695_rx_cpack/fifo_wr_data_0
+ad_connect rx_ad9695_tpl_core/adc_data_1 util_ad9695_rx_cpack/fifo_wr_data_1
+ad_connect rx_ad9695_tpl_core/adc_dovf util_ad9695_rx_cpack/fifo_wr_overflow
+
+# interconnect (cpu)
+
+ad_cpu_interconnect 0x44A00000 rx_ad9695_tpl_core
+ad_cpu_interconnect 0x44A60000 axi_ad9695_rx_xcvr
+ad_cpu_interconnect 0x44AA0000 axi_ad9695_rx_jesd
+ad_cpu_interconnect 0x7c400000 axi_ad9695_rx_dma
+
+ad_mem_hp0_interconnect $sys_cpu_clk sys_ps8/S_AXI_HP0
+ad_mem_hp0_interconnect $sys_cpu_clk axi_ad9695_rx_xcvr/m_axi
+
+# interconnect (mem/dac)
+
+ad_mem_hp2_interconnect dma_clk_wiz/clk_out1 sys_ps7/S_AXI_HP1
+ad_mem_hp2_interconnect dma_clk_wiz/clk_out1 axi_ad9695_rx_dma/m_dest_axi
+
+# interrupts
+
+ad_cpu_interrupt ps-12 mb-13 axi_ad9695_rx_jesd/irq
+ad_cpu_interrupt ps-13 mb-12 axi_ad9695_rx_dma/irq
+

projects/ad9695_fmc/zcu102/system_top.v
@@ -1,0 +1,167 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top (
+
+  input       [12:0]      gpio_bd_i,
+  output      [ 7:0]      gpio_bd_o,
+
+  input                   ref_clk0_p,
+  input                   ref_clk0_n,
+
+  input                   sysref_p,
+  input                   sysref_n,
+
+  input       [ 3:0]      rx_data_p,
+  input       [ 3:0]      rx_data_n,
+  output                  rx_sync_p,
+  output                  rx_sync_n,
+
+  output                  spi_csn,
+  output                  spi_clk,
+  output                  spi_mosi,
+  input                   spi_miso,
+
+  inout                   fda,
+  inout                   fdb,
+  inout                   pwdn,
+
+  output                  pmod_spi_csn,
+  output                  pmod_spi_mosi,
+  input                   pmod_spi_miso,
+  output                  pmod_spi_clk
+);
+
+  // internal signals
+
+  wire        [94:0]      gpio_i;
+  wire        [94:0]      gpio_o;
+  wire        [94:0]      gpio_t;
+  wire        [20:0]      gpio_bd;
+  wire                    ref_clk0;
+  wire                    rx_sync;
+  wire                    rx_ref_core_clk0_s;
+  wire                    rx_ref_core_clk0;
+  wire         [2:0]      spi1_csn;
+  wire                    sysref_s;
+  wire                    sysref_0;
+
+  assign gpio_i[94:35] = gpio_o[94:35];
+  assign gpio_i[31:21] = gpio_o[31:21];
+  assign gpio_i[ 7: 0] = gpio_o[ 7: 0];
+
+  assign gpio_i[20: 8] = gpio_bd_i;
+  assign gpio_bd_o = gpio_o[7:0];
+
+  assign pmod_spi_csn = spi1_csn[0];
+  // instantiations
+
+  ad_iobuf #(.DATA_WIDTH(3)) i_iobuf (
+    .dio_t ({gpio_t[34:32]}),
+    .dio_i ({gpio_o[34:32]}),
+    .dio_o ({gpio_i[34:32]}),
+    .dio_p ({fdb,        // 34
+             fda,        // 33
+             pwdn}));    // 32
+
+  IBUFDS_GTE4 i_ibufds_ref_clk0 (
+    .CEB (1'd0),
+    .I (ref_clk0_p),
+    .IB (ref_clk0_n),
+    .O (ref_clk0),
+    .ODIV2 (rx_ref_core_clk0_s));
+
+  BUFG_GT i_bufg_gt_ref_core_clk(
+    .I(rx_ref_core_clk0_s),
+    .CE (1'b1),
+    .CEMASK(1'b1),
+    .CLR (1'b0),
+    .CLRMASK(1'b1),
+    .DIV (3'b000),
+    .O(rx_ref_core_clk0));
+
+  IBUFDS_GTE4 i_ibufds_sysref (
+    .CEB (1'd0),
+    .I (sysref_p),
+    .IB (sysref_n),
+    .O (),
+    .ODIV2 (sysref_s));
+
+  BUFG_GT i_bufg_gt_sysref(
+    .I(sysref_s),
+    .CE (1'b1),
+    .CEMASK(1'b1),
+    .CLR (1'b0),
+    .CLRMASK(1'b1),
+    .DIV (3'b000),
+    .O(sysref_0));
+
+  OBUFDS i_obufds_rx_sync (
+    .I (rx_sync),
+    .O (rx_sync_p),
+    .OB (rx_sync_n));
+
+  system_wrapper i_system_wrapper (
+    .gpio_i (gpio_i),
+    .gpio_o (gpio_o),
+    .gpio_t (gpio_t),
+    .rx_data_0_n (rx_data_n[0]),
+    .rx_data_0_p (rx_data_p[0]),
+    .rx_data_1_n (rx_data_n[1]),
+    .rx_data_1_p (rx_data_p[1]),
+    .rx_data_2_n (rx_data_n[2]),
+    .rx_data_2_p (rx_data_p[2]),
+    .rx_data_3_n (rx_data_n[3]),
+    .rx_data_3_p (rx_data_p[3]),
+    .rx_ref_clk_0 (ref_clk0),
+    .rx_core_clk_0 (rx_ref_core_clk0),
+    .rx_sync_0 (rx_sync),
+    .rx_sysref_0 (sysref_0),
+    .spi0_sclk (spi_clk),
+    .spi0_csn (spi_csn),
+    .spi0_miso (spi_miso),
+    .spi0_mosi (spi_mosi),
+    .spi1_csn (spi1_csn),
+    .spi1_miso (pmod_spi_miso),
+    .spi1_mosi (pmod_spi_mosi),
+    .spi1_sclk (pmod_spi_clk)
+    );
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************

tausen/adi-hdl

projects/ad9695_fmc/common/ad9695_fmc_bd.tcl
@@ -1,0 +1,164 @@
+
+# RX parameters
+set RX_NUM_OF_LANES $ad_project_params(RX_JESD_L)           ; # L
+set RX_NUM_OF_CONVERTERS $ad_project_params(RX_JESD_M)      ; # M
+set RX_SAMPLES_PER_FRAME $ad_project_params(RX_JESD_S)      ; # S
+set RX_SAMPLE_WIDTH 16                                      ; # N/NP
+set RX_SAMPLES_PER_CHANNEL [expr $RX_NUM_OF_LANES * 32 / ($RX_NUM_OF_CONVERTERS * $RX_SAMPLE_WIDTH)]
+
+set adc_data_width [expr $RX_SAMPLE_WIDTH * $RX_NUM_OF_CONVERTERS * $RX_SAMPLES_PER_CHANNEL]
+set adc_dma_data_width [expr $RX_SAMPLE_WIDTH * $RX_NUM_OF_CONVERTERS * $RX_SAMPLES_PER_CHANNEL]
+
+# These are max values specific to the board
+set MAX_RX_NUM_OF_LANES 4
+
+source $ad_hdl_dir/library/jesd204/scripts/jesd204.tcl
+
+# adc peripherals
+
+ad_ip_instance axi_adxcvr axi_ad9695_rx_xcvr [list \
+  NUM_OF_LANES $RX_NUM_OF_LANES \
+  QPLL_ENABLE 1 \
+  TX_OR_RX_N 0 \
+  SYS_CLK_SEL 3 \
+  OUT_CLK_SEL 3 \
+  ]
+
+adi_axi_jesd204_rx_create axi_ad9695_rx_jesd $RX_NUM_OF_LANES
+ad_ip_parameter axi_ad9695_rx_jesd/rx CONFIG.SYSREF_IOB false
+
+ad_ip_instance util_cpack2 util_ad9695_rx_cpack [list \
+  NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS \
+  SAMPLES_PER_CHANNEL $RX_SAMPLES_PER_CHANNEL \
+  SAMPLE_DATA_WIDTH $RX_SAMPLE_WIDTH \
+  ]
+
+adi_tpl_jesd204_rx_create rx_ad9695_tpl_core $RX_NUM_OF_LANES \
+                                               $RX_NUM_OF_CONVERTERS \
+                                               $RX_SAMPLES_PER_FRAME \
+                                               $RX_SAMPLE_WIDTH
+
+ad_ip_instance clk_wiz dma_clk_wiz
+ad_ip_parameter dma_clk_wiz CONFIG.PRIMITIVE MMCM
+ad_ip_parameter dma_clk_wiz CONFIG.RESET_TYPE ACTIVE_LOW
+ad_ip_parameter dma_clk_wiz CONFIG.USE_LOCKED false
+ad_ip_parameter dma_clk_wiz CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 332.9
+ad_ip_parameter dma_clk_wiz CONFIG.PRIM_SOURCE No_buffer
+
+ad_ip_instance axi_dmac axi_ad9695_rx_dma [list \
+  DMA_TYPE_SRC 2 \
+  DMA_TYPE_DEST 0 \
+  CYCLIC 0 \
+  SYNC_TRANSFER_START 0 \
+  DMA_2D_TRANSFER 0 \
+  FIFO_SIZE 32 \
+  MAX_BYTES_PER_BURST 2048 \
+  AXI_SLICE_DEST 1 \
+  AXI_SLICE_SRC 1 \
+  DMA_LENGTH_WIDTH 24 \
+  DMA_DATA_WIDTH_DEST 128 \
+  DMA_DATA_WIDTH_SRC $adc_dma_data_width \
+  ]
+
+# common cores
+# fPLLClkin = 325 MHz => RX_CLK25_DIV = 13
+# fPLLClkout = 13000 MHz - qpll0
+
+ad_ip_instance util_adxcvr util_ad9695_xcvr [list \
+  RX_NUM_OF_LANES $MAX_RX_NUM_OF_LANES \
+  TX_NUM_OF_LANES 0 \
+  QPLL_FBDIV 40 \
+  QPLL_REFCLK_DIV 1 \
+  RX_OUT_DIV 1 \
+  RX_CLK25_DIV 13 \
+  POR_CFG 0x0 \
+  QPLL_CFG0 0x331c \
+  QPLL_CFG1 0xd038 \
+  QPLL_CFG1_G3 0xd038 \
+  QPLL_CFG2 0xfc1 \
+  QPLL_CFG2_G3 0xfc1 \
+  QPLL_CFG3 0x120 \
+  QPLL_CFG4 0x4 \
+  QPLL_CP 0xff \
+  QPLL_CP_G3 0xf \
+  QPLL_LPF 0x33f \
+  CH_HSPMUX 0x4444 \
+  PREIQ_FREQ_BST 1 \
+  RXPI_CFG0 0x104 \
+  RXPI_CFG1 0x0 \
+  RXCDR_CFG0 0x3 \
+  RXCDR_CFG2_GEN2 0x265 \
+  RXCDR_CFG2_GEN4 0x164 \
+  RXCDR_CFG3 0x18 \
+  RXCDR_CFG3_GEN2 0x18 \
+  RXCDR_CFG3_GEN3 0x18 \
+  RXCDR_CFG3_GEN4 0x12 \
+  ]
+
+# xcvr interfaces
+
+set rx_ref_clk     rx_ref_clk_0
+
+create_bd_port -dir I $rx_ref_clk
+create_bd_port -dir I rx_core_clk_0
+
+ad_connect  $sys_cpu_resetn util_ad9695_xcvr/up_rstn
+ad_connect  $sys_cpu_clk util_ad9695_xcvr/up_clk
+
+# Rx
+ad_connect ad9695_rx_device_clk rx_core_clk_0
+ad_xcvrcon  util_ad9695_xcvr axi_ad9695_rx_xcvr axi_ad9695_rx_jesd {} ad9695_rx_device_clk {} $MAX_RX_NUM_OF_LANES
+ad_xcvrpll $rx_ref_clk util_ad9695_xcvr/qpll_ref_clk_0
+for {set i 0} {$i < $MAX_RX_NUM_OF_LANES} {incr i} {
+  set ch [expr $i]
+  ad_xcvrpll  $rx_ref_clk util_ad9695_xcvr/cpll_ref_clk_$ch
+  ad_xcvrpll  axi_ad9695_rx_xcvr/up_pll_rst util_ad9695_xcvr/up_cpll_rst_$ch
+}
+
+ad_xcvrpll  axi_ad9695_rx_xcvr/up_pll_rst util_ad9695_xcvr/up_qpll_rst_*
+
+# connections (adc)
+
+ad_connect $sys_cpu_resetn axi_ad9695_rx_dma/m_dest_axi_aresetn
+ad_connect dma_clk_wiz/clk_out1 axi_ad9695_rx_dma/m_dest_axi_aclk
+ad_connect util_ad9695_rx_cpack/packed_fifo_wr axi_ad9695_rx_dma/fifo_wr
+
+ad_connect $sys_cpu_clk dma_clk_wiz/clk_in1
+ad_connect $sys_cpu_resetn dma_clk_wiz/resetn
+
+ad_connect ad9695_rx_device_clk rx_ad9695_tpl_core/link_clk
+ad_connect ad9695_rx_device_clk util_ad9695_rx_cpack/clk
+ad_connect ad9695_rx_device_clk axi_ad9695_rx_dma/fifo_wr_clk
+ad_connect ad9695_rx_device_clk_rstgen/peripheral_reset util_ad9695_rx_cpack/reset
+
+ad_connect axi_ad9695_rx_jesd/rx_sof rx_ad9695_tpl_core/link_sof
+ad_connect axi_ad9695_rx_jesd/rx_data_tdata rx_ad9695_tpl_core/link_data
+ad_connect axi_ad9695_rx_jesd/rx_data_tvalid rx_ad9695_tpl_core/link_valid
+
+ad_connect rx_ad9695_tpl_core/adc_valid_0 util_ad9695_rx_cpack/fifo_wr_en
+ad_connect rx_ad9695_tpl_core/adc_dovf util_ad9695_rx_cpack/fifo_wr_overflow
+for {set i 0} {$i < $RX_NUM_OF_CONVERTERS} {incr i} {
+  ad_connect rx_ad9695_tpl_core/adc_enable_$i util_ad9695_rx_cpack/enable_$i
+  ad_connect rx_ad9695_tpl_core/adc_data_$i util_ad9695_rx_cpack/fifo_wr_data_$i
+}
+
+# interconnect (cpu)
+
+ad_cpu_interconnect 0x44A00000 rx_ad9695_tpl_core
+ad_cpu_interconnect 0x44A60000 axi_ad9695_rx_xcvr
+ad_cpu_interconnect 0x44AA0000 axi_ad9695_rx_jesd
+ad_cpu_interconnect 0x7c400000 axi_ad9695_rx_dma
+
+ad_mem_hp0_interconnect $sys_cpu_clk sys_ps8/S_AXI_HP0
+ad_mem_hp0_interconnect $sys_cpu_clk axi_ad9695_rx_xcvr/m_axi
+
+# interconnect (mem/dac)
+
+ad_mem_hp2_interconnect dma_clk_wiz/clk_out1 sys_ps7/S_AXI_HP1
+ad_mem_hp2_interconnect dma_clk_wiz/clk_out1 axi_ad9695_rx_dma/m_dest_axi
+
+# interrupts
+
+ad_cpu_interrupt ps-12 mb-13 axi_ad9695_rx_jesd/irq
+ad_cpu_interrupt ps-13 mb-12 axi_ad9695_rx_dma/irq
+

projects/ad9695_fmc/zcu102/system_top.v
@@ -1,0 +1,170 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top (
+
+  input       [12:0]      gpio_bd_i,
+  output      [ 7:0]      gpio_bd_o,
+
+  input                   ref_clk0_p,
+  input                   ref_clk0_n,
+
+  input                   sysref_p,
+  input                   sysref_n,
+
+  input       [ 3:0]      rx_data_p,
+  input       [ 3:0]      rx_data_n,
+  output                  rx_sync_p,
+  output                  rx_sync_n,
+
+  output                  spi_csn,
+  output                  spi_clk,
+  output                  spi_mosi,
+  input                   spi_miso,
+
+  inout                   fda,
+  inout                   fdb,
+  inout                   pwdn,
+
+  output                  pmod_spi_csn,
+  output                  pmod_spi_mosi,
+  input                   pmod_spi_miso,
+  output                  pmod_spi_clk
+);
+
+  // internal signals
+
+  wire        [94:0]      gpio_i;
+  wire        [94:0]      gpio_o;
+  wire        [94:0]      gpio_t;
+  wire        [20:0]      gpio_bd;
+  wire                    ref_clk0;
+  wire                    rx_sync;
+  wire                    rx_ref_core_clk0_s;
+  wire                    rx_ref_core_clk0;
+  wire         [2:0]      spi0_csn;
+  wire         [2:0]      spi1_csn;
+  wire                    sysref_s;
+  wire                    sysref_0;
+
+  assign gpio_i[94:35] = gpio_o[94:35];
+  assign gpio_i[31:21] = gpio_o[31:21];
+  assign gpio_i[ 7: 0] = gpio_o[ 7: 0];
+
+  assign gpio_i[20: 8] = gpio_bd_i;
+  assign gpio_bd_o = gpio_o[7:0];
+
+  assign spi_csn = spi0_csn[0];
+  assign pmod_spi_csn = spi1_csn[0];
+
+  // instantiations
+
+  ad_iobuf #(.DATA_WIDTH(3)) i_iobuf (
+    .dio_t ({gpio_t[34:32]}),
+    .dio_i ({gpio_o[34:32]}),
+    .dio_o ({gpio_i[34:32]}),
+    .dio_p ({fdb,        // 34
+             fda,        // 33
+             pwdn}));    // 32
+
+  IBUFDS_GTE4 i_ibufds_ref_clk0 (
+    .CEB (1'd0),
+    .I (ref_clk0_p),
+    .IB (ref_clk0_n),
+    .O (ref_clk0),
+    .ODIV2 (rx_ref_core_clk0_s));
+
+  BUFG_GT i_bufg_gt_ref_core_clk(
+    .I(rx_ref_core_clk0_s),
+    .CE (1'b1),
+    .CEMASK(1'b1),
+    .CLR (1'b0),
+    .CLRMASK(1'b1),
+    .DIV (3'b000),
+    .O(rx_ref_core_clk0));
+
+  IBUFDS_GTE4 i_ibufds_sysref (
+    .CEB (1'd0),
+    .I (sysref_p),
+    .IB (sysref_n),
+    .O (),
+    .ODIV2 (sysref_s));
+
+  BUFG_GT i_bufg_gt_sysref(
+    .I(sysref_s),
+    .CE (1'b1),
+    .CEMASK(1'b1),
+    .CLR (1'b0),
+    .CLRMASK(1'b1),
+    .DIV (3'b000),
+    .O(sysref_0));
+
+  OBUFDS i_obufds_rx_sync (
+    .I (rx_sync),
+    .O (rx_sync_p),
+    .OB (rx_sync_n));
+
+  system_wrapper i_system_wrapper (
+    .gpio_i (gpio_i),
+    .gpio_o (gpio_o),
+    .gpio_t (gpio_t),
+    .rx_data_0_n (rx_data_n[0]),
+    .rx_data_0_p (rx_data_p[0]),
+    .rx_data_1_n (rx_data_n[1]),
+    .rx_data_1_p (rx_data_p[1]),
+    .rx_data_2_n (rx_data_n[2]),
+    .rx_data_2_p (rx_data_p[2]),
+    .rx_data_3_n (rx_data_n[3]),
+    .rx_data_3_p (rx_data_p[3]),
+    .rx_ref_clk_0 (ref_clk0),
+    .rx_core_clk_0 (rx_ref_core_clk0),
+    .rx_sync_0 (rx_sync),
+    .rx_sysref_0 (sysref_0),
+    .spi0_csn (spi0_csn),
+    .spi0_miso (spi_miso),
+    .spi0_mosi (spi_mosi),
+    .spi0_sclk (spi_clk),
+    .spi1_csn (spi1_csn),
+    .spi1_miso (pmod_spi_miso),
+    .spi1_mosi (pmod_spi_mosi),
+    .spi1_sclk (pmod_spi_clk)
+    );
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************